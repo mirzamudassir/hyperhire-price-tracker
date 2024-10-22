88ef5456f1a11dfd00b2c460774265f6ffa8225e
Mudassir Mirza <hello.mudassirmirza@gmail.com>
2024-10-22 18:47:18 +0500

    swagger configured, pg configured

---
+++ src/app.module.ts
@@ -0,0 +1,39 @@
+import { Module } from '@nestjs/common';
+import { AppController } from './app.controller';
+import { AppService } from './app.service';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { ConfigModule, ConfigService } from '@nestjs/config';
+
+@Module({
+  imports: [
+    ConfigModule.forRoot({
+      isGlobal: true,
+      envFilePath: '.env',
+    }),
+    TypeOrmModule.forRootAsync({
+      imports: [ConfigModule],
+      inject: [ConfigService],
+      useFactory: async (configService: ConfigService) => ({
+        type: 'postgres',
+        host: configService.get<string>('DATABASE_HOST'),
+        port: configService.get<number>('DATABASE_PORT'),
+        username: configService.get<string>('DATABASE_USERNAME'),
+        password: configService.get<string>('DATABASE_PASSWORD'),
+        database: configService.get<string>('DATABASE_NAME'),
+        synchronize: false,
+        entities: ['dist/**/*.entity{.ts,.js}'],
+        autoLoadEntities: true,
+        migrations: [__dirname + '/migrations/**/*{.ts,.js}'],
+        seeds: [__dirname + '/seeds/**/*{.ts,.js}'],
+        factories: [__dirname + '/factories/**/*{.ts,.js}'],
+        cli: {
+          migrationsDir: __dirname + '/migrations/',
+        },
+      }),
+    }),
+    // PriceModule,
+  ],
+  controllers: [AppController],
+  providers: [AppService],
+})
+export class AppModule {}
